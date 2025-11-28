c796c897f6cec108a2f8e8b9e7d29ef23cc2e02f
Samarth Patel <psamarth8836@gmail.com>
2025-11-28 13:25:21 +0530

    adding author name to already existing template, also got it from the context of the doctype

--- star_ebook_store/star_ebook_store/doctype/ebook/ebook.py
+++ star_ebook_store/star_ebook_store/doctype/ebook/ebook.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2025, Samarth and contributors
 # For license information, please see license.txt
 
-# import frappe
-from frappe.website.website_generator import WebsiteGenerator
+import frappe
+from frappe.website.website_generator import WebsiteGenerator # type: ignore
 
 
 class ebook(WebsiteGenerator):
-	pass
+	def get_context(self, context):
+		context.author = frappe.db.get_value(
+            "Author", self.author, ["full_name as name", "bio"], as_dict=True
+        )
