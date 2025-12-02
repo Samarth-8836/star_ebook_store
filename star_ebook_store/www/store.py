505f18b9c4f877a968b343816b6b54eb73b9027f
Samarth Patel <psamarth8836@gmail.com>
2025-12-02 15:50:57 +0530

    updating store.html to display more store contents and adding a standard store.py file to later add the context to the front page

---
+++ star_ebook_store/www/store.py
@@ -0,0 +1,19 @@
+import frappe
+
+
+def get_context(context):
+	# context.ebooks = frappe.get_all(
+	# 	"eBook",
+	# 	fields=[
+	# 		"name",
+	# 		"cover_image",
+	# 		"price_inr",
+	# 		"format",
+	# 		"route",
+	# 		"creation",
+	# 		"author.full_name as author_name",
+	# 	],
+	# 	filters={"is_published": 1},
+	# 	order_by="creation desc"
+	# )
+    pass
