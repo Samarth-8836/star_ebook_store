821ddf4a466dca1fa48c2bbafef1079e92e87f05
Samarth Patel <psamarth8836@gmail.com>
2025-12-02 19:04:12 +0530

    added correct context for front end store page to display everything needed

--- star_ebook_store/www/store.py
+++ star_ebook_store/www/store.py
@@ -2,18 +2,17 @@ import frappe
 
 
 def get_context(context):
-	# context.ebooks = frappe.get_all(
-	# 	"eBook",
-	# 	fields=[
-	# 		"name",
-	# 		"cover_image",
-	# 		"price_inr",
-	# 		"format",
-	# 		"route",
-	# 		"creation",
-	# 		"author.full_name as author_name",
-	# 	],
-	# 	filters={"is_published": 1},
-	# 	order_by="creation desc"
-	# )
-    pass
+	context.ebooks = frappe.get_all(
+		"ebook",
+		fields=[
+			"name",
+			"cover_image",
+			"price",
+			"format",
+			"route",
+			"creation",
+			"author.full_name as author_name",
+		],
+		filters={"is_published": 1},
+		order_by="creation desc"
+	)
